c0a235ad2bb033d802d57ff45c791a0460b15d05
hritik2004-cse <hs1349507@gmail.com>
2025-07-01 23:45:21 +0530

    first commit

---
+++ day_1.py
@@ -0,0 +1,12 @@
+# ✅ Tasks:
+# 🟣 Tuples
+# Create a tuple with 5 favorite movies.
+
+fav_movies = ("Stranger Things","Squid Game","Money Heist","Alice in Borderland","The Lion King");
+
+# Access and print the third movie.
+print(fav_movies[2]);
+
+#Try modifying one element of the tuple and observe the error (write the error message in a comment).
+fav_movies[1]="Avengers Endgame";
+print(fav_movies);#This will throw error
